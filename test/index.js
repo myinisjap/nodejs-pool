86345c6266e2ca0a08791a3b7c30357f26a3a40d
Stanimal <sdbondi@users.noreply.github.com>
2021-02-18 16:14:10 +0400

    wip4

--- test/index.js
+++ test/index.js
@@ -2,23 +2,26 @@ const assert = require('assert');
 
 // Setup global state
 global.config = {
-    general: {network: 'stagenet'}
+    general: { network: 'stagenet' },
 };
 
 describe('common', function () {
     describe('#tryParseMergeMiningAddress()', function () {
-        let {tryParseMergeMiningAddress} = require('../lib/common');
+        const { tryParseMergeMiningAddress } = require('../lib/common');
         it('returns address parts if valid', function () {
             const address =
-                "06b9998a8a6001ab17f359cd1371b78e1930f3d99d30f97d93889e3ca664ff2f:51skQCSE9dAJjWPe6RWojZ536oQHMLUYxZPei3B32YLTcdEX37X6xBR1PFwA3hmYciNz4kpC3cuuy6XH8SQV7f5E5AnKQkF";
-            const {tari, monero} = tryParseMergeMiningAddress(address);
-            assert.strictEqual(tari, "06b9998a8a6001ab17f359cd1371b78e1930f3d99d30f97d93889e3ca664ff2f");
-            assert.strictEqual(monero, "51skQCSE9dAJjWPe6RWojZ536oQHMLUYxZPei3B32YLTcdEX37X6xBR1PFwA3hmYciNz4kpC3cuuy6XH8SQV7f5E5AnKQkF");
+                '06b9998a8a6001ab17f359cd1371b78e1930f3d99d30f97d93889e3ca664ff2f:51skQCSE9dAJjWPe6RWojZ536oQHMLUYxZPei3B32YLTcdEX37X6xBR1PFwA3hmYciNz4kpC3cuuy6XH8SQV7f5E5AnKQkF';
+            const { tari, monero } = tryParseMergeMiningAddress(address);
+            assert.strictEqual(tari, '06b9998a8a6001ab17f359cd1371b78e1930f3d99d30f97d93889e3ca664ff2f');
+            assert.strictEqual(
+                monero,
+                '51skQCSE9dAJjWPe6RWojZ536oQHMLUYxZPei3B32YLTcdEX37X6xBR1PFwA3hmYciNz4kpC3cuuy6XH8SQV7f5E5AnKQkF',
+            );
         });
 
         it('errors if no delimiter', function () {
             const address =
-                "06b9998a8a6001ab17f359cd1371b78e1930f3d99d30f97d93889e3ca664ff2f_51skQCSE9dAJjWPe6RWojZ536oQHMLUYxZPei3B32YLTcdEX37X6xBR1PFwA3hmYciNz4kpC3cuuy6XH8SQV7f5E5AnKQkF";
+                '06b9998a8a6001ab17f359cd1371b78e1930f3d99d30f97d93889e3ca664ff2f_51skQCSE9dAJjWPe6RWojZ536oQHMLUYxZPei3B32YLTcdEX37X6xBR1PFwA3hmYciNz4kpC3cuuy6XH8SQV7f5E5AnKQkF';
             assert.throws(() => {
                 tryParseMergeMiningAddress(address);
             });
@@ -26,7 +29,7 @@ describe('common', function () {
 
         it('errors if invalid tari address', function () {
             const address =
-                "z6b9998a8a6001ab17f359cd1371b78e1930f3d99d30f97d93889e3ca664ff2f:51skQCSE9dAJjWPe6RWojZ536oQHMLUYxZPei3B32YLTcdEX37X6xBR1PFwA3hmYciNz4kpC3cuuy6XH8SQV7f5E5AnKQkF";
+                'z6b9998a8a6001ab17f359cd1371b78e1930f3d99d30f97d93889e3ca664ff2f:51skQCSE9dAJjWPe6RWojZ536oQHMLUYxZPei3B32YLTcdEX37X6xBR1PFwA3hmYciNz4kpC3cuuy6XH8SQV7f5E5AnKQkF';
             assert.throws(() => {
                 tryParseMergeMiningAddress(address);
             });
@@ -34,7 +37,7 @@ describe('common', function () {
 
         it('errors if invalid monero address', function () {
             const address =
-                "06b9998a8a6001ab17f359cd1371b78e1930f3d99d30f97d93889e3ca664ff2f:61skQCSE9dAJjWPe6RWojZ536oQHMLUYxZPei3B32YLTcdEX37X6xBR1PFwA3hmYciNz4kpC3cuuy6XH8SQV7f5E5AnKQkF";
+                '06b9998a8a6001ab17f359cd1371b78e1930f3d99d30f97d93889e3ca664ff2f:61skQCSE9dAJjWPe6RWojZ536oQHMLUYxZPei3B32YLTcdEX37X6xBR1PFwA3hmYciNz4kpC3cuuy6XH8SQV7f5E5AnKQkF';
             assert.throws(() => {
                 tryParseMergeMiningAddress(address);
             });
